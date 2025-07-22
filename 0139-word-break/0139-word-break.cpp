644aa65c86a48a6a59e7c1c9f699253456de7a63
Amritesh Raj <amriteshraj1798@gmail.com>
2025-07-21 19:55:52 -0700

    Time: 22 ms (15.61%), Space: 21.2 MB (12.14%) - LeetHub

--- 0139-word-break/0139-word-break.cpp
+++ 0139-word-break/0139-word-break.cpp
@@ -1,27 +1,27 @@
 class Solution {
 public:
-    int n ;
     unordered_set<string> st;
-    int dp[301];
-    bool solve(int idx , string s){
-
-        if(idx == n) return true;
-        if(dp[idx]!=-1) return dp[idx];
-
+    int n;
+    int t[301];
+    bool helper(string &s , int idx){
+        if(idx==n) return true;
         if(st.find(s)!=st.end()) return true;
-
-        for(int len = 1 ; len < n ; len++){
-            string temp = s.substr(idx,len);
-            if(st.find(temp)!=st.end() && solve(idx+len,s)){
-                return dp[idx] = true;
+        if(t[idx]!=-1) return t[idx];
+        for(int len=1;len<=n;len++){
+            string temp=s.substr(idx,len);
+            if(st.find(temp)!=st.end()&& helper(s,idx+len)){
+                return t[idx]=true;
             }
+
         }
-        return dp[idx] = false;
+        return t[idx]=false;
     }
     bool wordBreak(string s, vector<string>& wordDict) {
-        n = s.length();
-        for(auto ch : wordDict) st.insert(ch);
-        memset(dp,-1,sizeof(dp));
-        return solve(0,s);
+        n=s.length();
+        memset(t, -1, sizeof(t));
+       for(auto &it :wordDict){
+            st.insert(it);
+       }
+        return helper(s,0);
     }
 };
